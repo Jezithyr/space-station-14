427f76221d37a475c790d0a2276ec1d0e1573536
Jezithyr <Jezithyr@gmail.com>
2023-11-29 14:12:10 -0800

    interm

---
+++ Content.Shared/Medical/Treatments/Prototypes/TreatmentPrototype.cs
@@ -0,0 +1,27 @@
+﻿using Content.Shared.FixedPoint;
+using Robust.Shared.Prototypes;
+using Robust.Shared.Serialization.TypeSerializers.Implementations.Custom.Prototype;
+
+namespace Content.Shared.Medical.Treatments.Prototypes;
+
+
+[Prototype("treatment")]
+public sealed class TreatmentPrototype : IPrototype
+{
+
+    [IdDataField] public string ID { get; init; } = string.Empty;
+
+    [DataField("repeatable")] public bool Repeatable = false;
+
+    [DataField("severityChange", required: true)]
+    public FixedPoint2 SeverityDelta;
+
+    [DataField("healingSpeedChange", required: true)]
+    public FixedPoint2 HealingSpeedDelta;
+
+    //TODO: raise treatment event
+
+    [DataField("allowedWounds", required: true, customTypeSerializer: typeof(PrototypeIdSerializer<EntityPrototype>))]
+    public HashSet<EntityPrototype> AllowedWounds = new();
+
+}
--- Content.Shared/Medical/Wounds/Systems/WoundSystem.cs
+++ Content.Shared/Medical/Wounds/Systems/WoundSystem.cs
@@ -1,10 +1,5 @@
-﻿using System.Linq;
-using Content.Shared.Body.Part;
-using Content.Shared.Body.Systems;
-using Content.Shared.Containers;
+﻿using Content.Shared.Body.Systems;
 using Content.Shared.Damage;
-using Content.Shared.Medical.Wounds.Components;
-using Content.Shared.Rejuvenate;
 using Content.Shared.Throwing;
 using Robust.Shared.Containers;
 using Robust.Shared.Network;
@@ -23,7 +18,6 @@ public partial class WoundSystem : EntitySystem
     [Dependency] private readonly INetManager _net = default!;
     [Dependency] private readonly IRobustRandom _random = default!;
     [Dependency] private readonly SharedContainerSystem _container = default!;
-    [Dependency] private readonly ThrowingSystem _throwing = default!;
 
 
     public override void Initialize()
